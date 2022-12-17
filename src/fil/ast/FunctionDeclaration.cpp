0b1275baba4c2e02fb744fe4141ad3736bb942a6
Kevin Traini <kevin@ktraini.com>
2022-12-17 09:45:53 +0100

    Fix ast::FunctionDeclaration::decompile

--- src/fil/ast/FunctionDeclaration.cpp
+++ src/fil/ast/FunctionDeclaration.cpp
@@ -23,6 +23,15 @@ FunctionDeclaration::~FunctionDeclaration() {
 string FunctionDeclaration::decompile(int indent) const {
     string result = _name->decompile(indent);
 
+    result += "(";
+    for (auto it = _params.begin(); it != _params.end(); ++it) {
+        result += (*it)->decompile(indent);
+        if (it + 1 != _params.end()) {
+            result += ", ";
+        }
+    }
+    result += ")";
+
     if (_type) {
         result += " : " + _type->decompile(indent);
     }
