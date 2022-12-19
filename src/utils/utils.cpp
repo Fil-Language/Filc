29267cec2821723a5e5b26bbfe8f924346c1c619
Kevin Traini <kevin@ktraini.com>
2022-12-19 16:08:12 +0100

    Add class Position

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -6,6 +6,36 @@
  */
 #include "utils.h"
 
+#include <fstream>
+
 std::string &ltrim(std::string &input) {
     return input.erase(0, input.find_first_not_of(' '));
+}
+
+Position::Position(int line, int column, const std::string &filename)
+        : _line(line), _column(column), _filename(filename) {}
+
+int Position::getLine() const {
+    return _line;
+}
+
+int Position::getColumn() const {
+    return _column;
+}
+
+std::string Position::getFilename() const {
+    return _filename;
+}
+
+std::string Position::getLineContent() const {
+    std::ifstream file(_filename);
+    std::string line;
+
+    for (int i = 0; i < _line; i++) {
+        std::getline(file, line);
+    }
+
+    file.close();
+
+    return line;
 }
--- src/utils/utils.h
+++ src/utils/utils.h
@@ -9,6 +9,43 @@
 
 #include <string>
 
+/**
+ * Trim left
+ *
+ * @param input
+ * @return
+ */
 std::string &ltrim(std::string &input);
+
+/**
+ * Describe a position in a file
+ */
+class Position {
+public:
+    /**
+     * Main constructor
+     *
+     * @param line
+     * @param column
+     * @param filename
+     */
+    Position(int line, int column, const std::string &filename);
+
+    int getLine() const;
+
+    int getColumn() const;
+
+    std::string getFilename() const;
+
+    /**
+     * @return The line in the file where the position is
+     */
+    std::string getLineContent() const;
+
+private:
+    int _line;
+    int _column;
+    std::string _filename;
+};
 
 #endif //FILC_UTILS_H
