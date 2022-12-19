03ecf04bb390188f8d97e860f025f9fc599d9b1c
Kevin Traini <kevin@ktraini.com>
2022-12-19 16:19:20 +0100

    Add 2 SymbolTables to Environment (functions & variables)

--- src/fil/environment/Environment.cpp
+++ src/fil/environment/Environment.cpp
@@ -7,14 +7,35 @@
 #include "Environment.h"
 
 Environment::Environment(Environment *parent)
-        : _parent(parent) {}
+        : _parent(parent) {
+    _functions = new SymbolTable();
+    _variables = new SymbolTable();
+}
 
 Environment::~Environment() {
     delete _parent;
+    delete _functions;
+    delete _variables;
 }
 
 Environment *Environment::getGlobalEnvironment() {
     static auto *globalEnvironment = new Environment();
 
     return globalEnvironment;
+}
+
+bool Environment::addFunction(const std::string &name, Position *position) {
+    return _functions->addSymbol(name, position);
+}
+
+bool Environment::addVariable(const std::string &name, Position *position) {
+    return _variables->addSymbol(name, position);
+}
+
+bool Environment::hasFunction(const std::string &name) const {
+    return _functions->hasSymbol(name);
+}
+
+bool Environment::hasVariable(const std::string &name) const {
+    return _variables->hasSymbol(name);
 }
--- src/fil/environment/Environment.h
+++ src/fil/environment/Environment.h
@@ -7,6 +7,7 @@
 #ifndef FILC_ENVIRONMENT_H
 #define FILC_ENVIRONMENT_H
 
+#include "SymbolTable.h"
 
 class Environment {
 public:
@@ -16,8 +17,19 @@ public:
 
     static Environment *getGlobalEnvironment();
 
+    bool addFunction(const std::string &name, Position *position);
+
+    bool addVariable(const std::string &name, Position *position);
+
+    bool hasFunction(const std::string &name) const;
+
+    bool hasVariable(const std::string &name) const;
+
 private:
     Environment *_parent;
+
+    SymbolTable *_functions;
+    SymbolTable *_variables;
 };
 
 
