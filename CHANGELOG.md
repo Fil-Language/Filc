19ee5d384bd63b7f567326a27c81d7db09f188af
Kevin Traini <kevin@ktraini.com>
2023-03-07 19:19:13 +0100

    Change version to 0.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## v0.3.0
+## v0.2.1
 
 - Add `--debug` option to compile with debug information
 - Add `-o,--out` option to specify executable name
--- src/VERSION.h
+++ src/VERSION.h
@@ -8,8 +8,8 @@
 #define FILC_VERSION_H
 
 #define FILC_VERSION_MAJOR 0
-#define FILC_VERSION_MINOR 3
-#define FILC_VERSION_PATCH 0
+#define FILC_VERSION_MINOR 2
+#define FILC_VERSION_PATCH 1
 
 #define MAKE_VERSION(major, minor, patch) ((major) * 1000000 + (minor) * 1000 + (patch))
 #define MAKE_VERSION_STRING(major, minor, patch) std::to_string(major) + "." + std::to_string(minor) + "." + std::to_string(patch)
