963edb46e48a70cb3325a75a30522538b2591319
Kevin Traini <kevin@ktraini.com>
2023-12-23 09:32:28 +0100

    test: Add more tests to ClassicOperator
    Part of #58

--- tests/unit/ast/ClassicOperatorTest.cpp
+++ tests/unit/ast/ClassicOperatorTest.cpp
@@ -22,9 +22,142 @@
  * SOFTWARE.
  */
 #include "AST.h"
-#include <gtest/gtest.h>
+#include "test_tools.h"
 
 TEST(ClassicOperator, constructor) {
     filc::ast::ClassicOperator co1(filc::ast::ClassicOperator::PLUSPLUS);
     ASSERT_EQ(filc::ast::ClassicOperator::PLUSPLUS, co1.getOperator());
+}
+
+TEST(ClassicOperator, dump) {
+    ASSERT_STREQ("++", filc::ast::ClassicOperator(filc::ast::ClassicOperator::PLUSPLUS).dump().c_str());
+    ASSERT_STREQ("--", filc::ast::ClassicOperator(filc::ast::ClassicOperator::MINUSMINUS).dump().c_str());
+    ASSERT_STREQ("+", filc::ast::ClassicOperator(filc::ast::ClassicOperator::PLUS).dump().c_str());
+    ASSERT_STREQ("-", filc::ast::ClassicOperator(filc::ast::ClassicOperator::MINUS).dump().c_str());
+    ASSERT_STREQ("&", filc::ast::ClassicOperator(filc::ast::ClassicOperator::REF).dump().c_str());
+    ASSERT_STREQ("*", filc::ast::ClassicOperator(filc::ast::ClassicOperator::STAR).dump().c_str());
+    ASSERT_STREQ("!", filc::ast::ClassicOperator(filc::ast::ClassicOperator::NOT).dump().c_str());
+    ASSERT_STREQ("/", filc::ast::ClassicOperator(filc::ast::ClassicOperator::DIV).dump().c_str());
+    ASSERT_STREQ("%", filc::ast::ClassicOperator(filc::ast::ClassicOperator::MOD).dump().c_str());
+    ASSERT_STREQ("<<", filc::ast::ClassicOperator(filc::ast::ClassicOperator::FLEFT).dump().c_str());
+    ASSERT_STREQ(">>", filc::ast::ClassicOperator(filc::ast::ClassicOperator::FRIGHT).dump().c_str());
+    ASSERT_STREQ("<", filc::ast::ClassicOperator(filc::ast::ClassicOperator::LESS).dump().c_str());
+    ASSERT_STREQ(">", filc::ast::ClassicOperator(filc::ast::ClassicOperator::GREATER).dump().c_str());
+    ASSERT_STREQ("==", filc::ast::ClassicOperator(filc::ast::ClassicOperator::EQEQ).dump().c_str());
+    ASSERT_STREQ("<=", filc::ast::ClassicOperator(filc::ast::ClassicOperator::LEQ).dump().c_str());
+    ASSERT_STREQ(">=", filc::ast::ClassicOperator(filc::ast::ClassicOperator::GEQ).dump().c_str());
+    ASSERT_STREQ("!=", filc::ast::ClassicOperator(filc::ast::ClassicOperator::NEQ).dump().c_str());
+    ASSERT_STREQ("&&", filc::ast::ClassicOperator(filc::ast::ClassicOperator::AND).dump().c_str());
+    ASSERT_STREQ("||", filc::ast::ClassicOperator(filc::ast::ClassicOperator::OR).dump().c_str());
+
+    auto non_existing = (filc::ast::ClassicOperator::OPERATOR) -1;
+    ASSERT_THROW(filc::ast::ClassicOperator(non_existing).dump(), std::logic_error);
+}
+
+TEST(ClassicOperator, dumpPreLambdaType) {
+    auto env = filc::environment::Environment("env", filc::environment::Environment::getGlobalEnvironment());
+    auto type = env.getType("int");
+
+    ASSERT_TYPE("(int*) -> int*", filc::ast::ClassicOperator(filc::ast::ClassicOperator::PLUSPLUS).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TYPE("(int*) -> int*", filc::ast::ClassicOperator(filc::ast::ClassicOperator::MINUSMINUS).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TYPE("(int) -> int", filc::ast::ClassicOperator(filc::ast::ClassicOperator::PLUS).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TYPE("(int) -> int", filc::ast::ClassicOperator(filc::ast::ClassicOperator::MINUS).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TYPE("(int) -> int", filc::ast::ClassicOperator(filc::ast::ClassicOperator::REF).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TYPE("(int) -> int", filc::ast::ClassicOperator(filc::ast::ClassicOperator::STAR).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TYPE("(int) -> int", filc::ast::ClassicOperator(filc::ast::ClassicOperator::NOT).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::DIV).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::MOD).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::FLEFT).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::FRIGHT).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::LESS).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::GREATER).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::EQEQ).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::LEQ).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::GEQ).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::NEQ).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::AND).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::OR).dumpPreLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+}
+
+TEST(ClassicOperator, dumpPostLambdaType) {
+    auto env = filc::environment::Environment("env", filc::environment::Environment::getGlobalEnvironment());
+    auto type = env.getType("int");
+
+    ASSERT_TYPE("(int*) -> int", filc::ast::ClassicOperator(filc::ast::ClassicOperator::PLUSPLUS).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TYPE("(int*) -> int", filc::ast::ClassicOperator(filc::ast::ClassicOperator::MINUSMINUS).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::PLUS).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::MINUS).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::REF).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::STAR).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::NOT).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::DIV).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::MOD).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::FLEFT).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::FRIGHT).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::LESS).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::GREATER).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::EQEQ).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::LEQ).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::GEQ).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::NEQ).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::AND).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
+    ASSERT_EQ(nullptr, filc::ast::ClassicOperator(filc::ast::ClassicOperator::OR).dumpPostLambdaType(type, &env, COLLECTOR, nullptr));
+    ASSERT_TRUE(COLLECTOR->hasErrors());
+    COLLECTOR->flush();
 }
